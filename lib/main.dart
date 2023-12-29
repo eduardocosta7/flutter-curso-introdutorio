737cdb00899a6079181a37b870170fca163ea5d6
Eduardo Feitosa Costa <e.feitosa@spotpromo.com.br>
2023-12-29 14:54:33 -0300

    primeiro desafio concluido

--- lib/main.dart
+++ lib/main.dart
@@ -57,18 +57,18 @@ class MyApp extends StatelessWidget {
                 children: [
                   Container(
                     color: Colors.cyanAccent,
-                    height: 50,
-                    width: 50,
+                    height: 100,
+                    width: 100,
                   ),
                   Container(
                     color: Colors.pinkAccent,
-                    height: 50,
-                    width: 50,
+                    height: 100,
+                    width: 100,
                   ),
                   Container(
                     color: Colors.purpleAccent,
-                    height: 50,
-                    width: 50,
+                    height: 100,
+                    width: 100,
                   )
                 ],
               ),
@@ -93,5 +93,92 @@ class MyApp extends StatelessWidget {
             ],
           ),
         ));
+  }
+}
+
+class ChallengeOne extends StatelessWidget {
+  const ChallengeOne({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Desafio 1',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigoAccent),
+        useMaterial3: true
+      ),
+      home: Container(
+        color: Colors.black,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  color: Colors.redAccent,
+                  width: 100,
+                  height: 100,
+                ),
+                Container(
+                  color: Colors.orangeAccent,
+                  width: 100,
+                  height: 100,
+                ),
+                Container(
+                  color: Colors.yellowAccent,
+                  width: 100,
+                  height: 100,
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  color: Colors.lightGreenAccent,
+                  width: 100,
+                  height: 100,
+                ),
+                Container(
+                  color: Colors.lightBlueAccent,
+                  width: 100,
+                  height: 100,
+                ),
+                Container(
+                  color: Colors.blueAccent,
+                  width: 100,
+                  height: 100,
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  color: Colors.purpleAccent,
+                  width: 100,
+                  height: 100,
+                ),
+                Container(
+                  color: Colors.pinkAccent,
+                  width: 100,
+                  height: 100,
+                ),
+                Container(
+                  color: Colors.white,
+                  width: 100,
+                  height: 100,
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
