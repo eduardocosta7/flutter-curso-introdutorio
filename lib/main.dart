c7700597b938d322522e2bad3cb21d7e3cd3beae
Eduardo Feitosa Costa <e.feitosa@spotpromo.com.br>
2023-12-29 14:09:13 -0300

    aplicando linhas e colunas

--- lib/main.dart
+++ lib/main.dart
@@ -15,13 +15,35 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Stack(
-        alignment: AlignmentDirectional.center,
+      home: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Container(color: Colors.redAccent, width: 200, height: 200,),
-          Container(color: Colors.blueAccent, width: 100, height: 100,)
+          Stack(
+            alignment: AlignmentDirectional.center,
+            children: [
+              Container(color: Colors.redAccent, width: 200, height: 200,),
+              Container(color: Colors.blueAccent, width: 100, height: 100,)
+            ],
+          ),
+          Stack(
+            alignment: AlignmentDirectional.center,
+            children: [
+              Container(color: Colors.blueAccent, width: 200, height: 200,),
+              Container(color: Colors.redAccent, width: 100, height: 100,)
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(color: Colors.cyanAccent, height: 50, width: 50,),
+              Container(color: Colors.pinkAccent, height: 50, width: 50,),
+              Container(color: Colors.purpleAccent, height: 50, width: 50,)
+            ],
+          )
         ],
-      ),
+      )
     );
   }
 }
