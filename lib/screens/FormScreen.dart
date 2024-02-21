7441dad94f3373888bd8a7ef1e15cb053d5c91e6
Eduardo Feitosa Costa <e.feitosa@spotpromo.com.br>
2024-02-20 23:01:58 -0300

    implementando um textFormField

--- lib/screens/FormScreen.dart
+++ lib/screens/FormScreen.dart
@@ -14,14 +14,18 @@ class FormScreen extends StatelessWidget {
           height: 650,
           width: 375,
           decoration: BoxDecoration(
-            color: Colors.black12,
-            borderRadius: BorderRadius.circular(10),
-            border: Border.all(
-              color: Colors.black,
-              width: 3
-            )
+              color: Colors.black12,
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(color: Colors.black, width: 3)),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              controller: TextEditingController(),
+              textAlign: TextAlign.start,
+              decoration: const InputDecoration(
+                  fillColor: Colors.white70, filled: true, hintText: "Nome"),
+            ),
           ),
-          child: ,
         ),
       ),
     );
